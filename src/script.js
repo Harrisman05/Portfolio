6f2e4b7c26f77a8f4dee005b5431543be70f34d9
harle <harleyharris05@hotmail.com>
2022-02-07 21:56:17 +0000

    added intersection observer for timeline

--- src/script.js
+++ src/script.js
@@ -86,3 +86,36 @@ function init() {
   // Init Typewriter
   new TypeWriter(txtElement, words, wait);
 }
+
+// Intersection Observer - Time line - https://www.youtube.com/watch?v=huVJW23JHKQ&t=33s
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
